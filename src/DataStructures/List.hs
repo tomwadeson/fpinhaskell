cc091ce0ae465095133590481f5bded7638559a4
Tom Wadeson <tom@tomwadeson.com>
2016-03-10 21:22:44 +0000

    Solve exercise 3.3

--- src/DataStructures/List.hs
+++ src/DataStructures/List.hs
@@ -1,6 +1,6 @@
 module DataStructures.List where
 
-import Prelude hiding (tail)
+import Prelude hiding (tail, head)
 
 data List a = Cons a (List a)
             | Nil
@@ -9,7 +9,16 @@ data List a = Cons a (List a)
 fromList :: [a] -> List a
 fromList = foldr Cons Nil
 
+head :: List a -> a
+head Nil        = undefined
+head (Cons x _) = x
+
 -- Ex 3.2
 tail :: List a -> List a
 tail Nil         = undefined
 tail (Cons _ xs) = xs
+
+-- Ex 3.3
+setHead :: List a -> a -> List a
+setHead Nil _         = undefined
+setHead (Cons _ xs) x = Cons x xs
--- test/DataStructures/ListSpec.hs
+++ test/DataStructures/ListSpec.hs
@@ -1,6 +1,6 @@
 module DataStructures.ListSpec where
 
-import Prelude hiding (tail)
+import Prelude hiding (tail, head)
 
 import Test.Hspec
 import Control.Exception (evaluate)
@@ -20,3 +20,19 @@ spec = do
       it "returns all but the first element" $ do
         let list = fromList [1..3]
         tail list `shouldBe` Cons 2 (Cons 3 Nil)
+  describe "head" $ do
+    context "empty list" $ do
+      it "is undefined" $ do
+        evaluate (head Nil) `shouldThrow` anyErrorCall
+    context "non-empty list" $ do
+      it "returns the first element in the list" $ do
+        let list = fromList [1..5]
+        head list `shouldBe` 1
+  describe "setHead" $ do
+    context "empty list" $ do
+      it "is undefined" $ do
+        evaluate (setHead Nil 1) `shouldThrow` anyErrorCall
+    context "non-empty list" $ do
+      it "returns the first element in the list" $ do
+        let list = fromList [1..3]
+        setHead list 100 `shouldBe` Cons 100 (Cons 2 (Cons 3 Nil))
