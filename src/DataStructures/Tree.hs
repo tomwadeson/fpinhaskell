db0d3bd8a5789fbce84df41682bdff91d5f1d016
Tom Wadeson <tom@tomwadeson.com>
2016-03-13 10:17:56 +0000

    Solve exercises 3.27-3.39

--- src/DataStructures/Tree.hs
+++ src/DataStructures/Tree.hs
@@ -1,6 +1,6 @@
 module DataStructures.Tree where
 
-import Prelude hiding (maximum)
+import Prelude hiding (maximum, map)
 
 data Tree a = Leaf a
             | Branch { left :: Tree a, right :: Tree a }
@@ -15,3 +15,30 @@ size (Branch l r) = 1 + size l + size r
 maximum :: Tree Int -> Int
 maximum (Leaf x)     = x
 maximum (Branch l r) = max (maximum l) (maximum r)
+
+-- Ex 3.27
+depth :: Tree a -> Int
+depth (Leaf _)     = 0
+depth (Branch l r) = 1 + max (depth l) (depth r) 
+
+-- Ex 3.28
+map :: (a -> b) -> Tree a -> Tree b
+map f (Leaf x)     = Leaf (f x)
+map f (Branch l r) = Branch (map f l) (map f r)
+
+-- Ex 3.29
+fold :: (a -> b) -> (b -> b -> b) -> Tree a -> b
+fold f _ (Leaf x)     = f x
+fold f g (Branch l r) = g (fold f g l) (fold f g r)
+
+size' :: Tree a -> Int
+size' = fold (const 1) (\x y -> 1 + x + y)
+
+maximum' :: Tree Int -> Int
+maximum' = fold id max
+
+depth' :: Tree a -> Int
+depth' = fold (const 0) (\x y -> 1 + max x y)
+
+map' :: (a -> b) -> Tree a -> Tree b
+map' f = fold (Leaf . f) Branch
--- test/DataStructures/TreeSpec.hs
+++ test/DataStructures/TreeSpec.hs
@@ -1,6 +1,6 @@
 module DataStructures.TreeSpec where
 
-import Prelude hiding (maximum)
+import Prelude hiding (maximum, map)
 
 import Test.Hspec
 import DataStructures.Tree
@@ -11,7 +11,19 @@ spec = do
     it "gives the size of a tree in terms of the count of its nodes" $ do
       let tree = Branch (Leaf 10) (Leaf 20)
       size tree `shouldBe` 3
+      size' tree `shouldBe` 3
   describe "maximum" $ do
     it "finds the maximum value contained in a Tree Int" $ do
       let tree = Branch (Leaf 1) (Branch (Leaf 2) (Leaf 5))
       maximum tree `shouldBe` 5
+      maximum' tree `shouldBe` 5
+  describe "depth" $ do
+    it "returns the maximum path length from the root of a tree to the deepest leaf" $ do
+      let tree = Branch (Leaf 1) (Branch (Leaf 2) (Leaf 5))
+      depth tree `shouldBe` 2
+      depth' tree `shouldBe` 2
+  describe "map" $ do
+    it "applies a function to each leaf in a tree, preserving its structure" $ do
+      let tree = Branch (Leaf 1) (Branch (Leaf 2) (Leaf 5))
+      map (*2) tree `shouldBe` Branch (Leaf 2) (Branch (Leaf 4) (Leaf 10))
+      map' (*2) tree `shouldBe` Branch (Leaf 2) (Branch (Leaf 4) (Leaf 10))
