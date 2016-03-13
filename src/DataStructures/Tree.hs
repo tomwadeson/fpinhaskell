781133702b02f1f6dd132e63cf8c3eba4b91ad5e
Tom Wadeson <tom@tomwadeson.com>
2016-03-13 09:44:39 +0000

    Solve exercise 3.26

--- src/DataStructures/Tree.hs
+++ src/DataStructures/Tree.hs
@@ -1,9 +1,17 @@
 module DataStructures.Tree where
 
+import Prelude hiding (maximum)
+
 data Tree a = Leaf a
-            | Branch { left :: Tree a, right :: Tree a}
+            | Branch { left :: Tree a, right :: Tree a }
             deriving (Eq, Show)
 
+-- Ex 3.25
 size :: Tree a -> Int
 size (Leaf _)     = 1
 size (Branch l r) = 1 + size l + size r
+
+-- Ex 3.26
+maximum :: Tree Int -> Int
+maximum (Leaf x)     = x
+maximum (Branch l r) = max (maximum l) (maximum r)
--- test/DataStructures/TreeSpec.hs
+++ test/DataStructures/TreeSpec.hs
@@ -1,5 +1,7 @@
 module DataStructures.TreeSpec where
 
+import Prelude hiding (maximum)
+
 import Test.Hspec
 import DataStructures.Tree
 
@@ -9,3 +11,7 @@ spec = do
     it "gives the size of a tree in terms of the count of its nodes" $ do
       let tree = Branch (Leaf 10) (Leaf 20)
       size tree `shouldBe` 3
+  describe "maximum" $ do
+    it "finds the maximum value contained in a Tree Int" $ do
+      let tree = Branch (Leaf 1) (Branch (Leaf 2) (Leaf 5))
+      maximum tree `shouldBe` 5
